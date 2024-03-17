5ef37b8e95aee161ad544a588824d5eaf23a6f7f
huangyqs123 <yanqiao@u.nus.edu>
2024-03-17 22:48:05 +0800

    cartographer

---
+++ src/carto_ws/install_isolated/share/gennodejs/ros/cartographer_ros_msgs/srv/_index.js
@@ -0,0 +1,20 @@
+
+"use strict";
+
+let StartTrajectory = require('./StartTrajectory.js')
+let SubmapQuery = require('./SubmapQuery.js')
+let ReadMetrics = require('./ReadMetrics.js')
+let WriteState = require('./WriteState.js')
+let GetTrajectoryStates = require('./GetTrajectoryStates.js')
+let TrajectoryQuery = require('./TrajectoryQuery.js')
+let FinishTrajectory = require('./FinishTrajectory.js')
+
+module.exports = {
+  StartTrajectory: StartTrajectory,
+  SubmapQuery: SubmapQuery,
+  ReadMetrics: ReadMetrics,
+  WriteState: WriteState,
+  GetTrajectoryStates: GetTrajectoryStates,
+  TrajectoryQuery: TrajectoryQuery,
+  FinishTrajectory: FinishTrajectory,
+};

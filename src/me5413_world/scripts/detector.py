159853e084a990a34513168cfcdaf463035f9b07
ztony0712 <ztony0712@outlook.com>
2024-04-03 22:03:13 +0800

    update

--- src/me5413_world/scripts/detector.py
+++ src/me5413_world/scripts/detector.py
@@ -1,11 +1,8 @@
-#!usr/bin/env python
+#!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
-import numpy as np
-from queue import Queue
-from threading import Thread
 from std_msgs.msg import String
 from vision_msgs.msg import Detection2D
 import os
